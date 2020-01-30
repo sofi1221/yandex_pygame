1bb7b284f4630132c7fde2683191125568cb4297
sofi1221 <sofi1221@mail.ru>
2020-01-30 13:46:17 +0500

    Starting window

--- project.py
+++ project.py
@@ -1,1 +1,21 @@
-print('Hello')
+import pygame
+
+pygame.init()
+size = width, height = 1100, 700
+screen = pygame.display.set_mode(size)
+
+
+def draw():
+    pass
+
+
+pygame.display.flip()
+running = True
+clock = pygame.time.Clock()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    draw()
+    pygame.display.flip()
+    clock.tick(24)
