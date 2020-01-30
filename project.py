15f8284d60e3869cd2b0ad05cad270dbcc0b49a3
sofi1221 <sofi1221@mail.ru>
2020-01-30 13:48:03 +0500

    + function draw()

--- project.py
+++ project.py
@@ -6,7 +6,14 @@ screen = pygame.display.set_mode(size)
 
 
 def draw():
-    pass
+    # screen.fill((204, 202, 202))  # светлая тема
+    screen.fill((57, 57, 57))  # темная тема
+    pygame.draw.rect(screen, pygame.Color(87, 87, 87), (150, 0, 500, 700), 0)
+    # pygame.draw.rect(screen, pygame.Color(230, 230, 230), (150, 0, 500, 700), 0)
+    pygame.draw.rect(screen, pygame.Color(147, 147, 147), (150, 340, 500, 20), 0)
+    pygame.draw.circle(screen, (147, 147, 147), (400, 350), 64)
+    pygame.draw.rect(screen, pygame.Color(255, 13, 0), (300, 680, 200, 20), 0)
+    pygame.draw.rect(screen, pygame.Color(51, 61, 255), (300, 0, 200, 20), 0)
 
 
 pygame.display.flip()
