90a923939478d3e7bc85537ceaea85af1d21635c
sofi1221 <sofi1221@mail.ru>
2020-01-30 14:02:43 +0500

    Добавлены классы

--- project.py
+++ project.py
@@ -1,30 +1,42 @@
 import pygame
+import random
 
 pygame.init()
 size = width, height = 1100, 700
 screen = pygame.display.set_mode(size)
+pos = (400, 350 + 175)
+pos_vrag = (400, 175)
+pos_shaiba = (400, 350)
+x = 0
+v_x = 0
+v_y = 0
+n = 0
+
+schet_igr = 0
+schet_comp = 0
+obsh_igr = 0
+obsh_comp = 0
+
+theme = 0
+level = 0
+
+screen.fill((87, 87, 87))
 
 
-def draw():
-    # screen.fill((204, 202, 202))  # светлая тема
-    screen.fill((57, 57, 57))  # темная тема
-    pygame.draw.rect(screen, pygame.Color(87, 87, 87), (150, 0, 500, 700), 0)
-    # pygame.draw.rect(screen, pygame.Color(230, 230, 230), (150, 0, 500, 700), 0)
-    pygame.draw.rect(screen, pygame.Color(147, 147, 147), (150, 340, 500, 20), 0)
-    pygame.draw.circle(screen, (147, 147, 147), (400, 350), 64)
-    pygame.draw.rect(screen, pygame.Color(255, 13, 0), (300, 680, 200, 20), 0)
-    pygame.draw.rect(screen, pygame.Color(51, 61, 255), (300, 0, 200, 20), 0)
-
-def otbit():
-    global v_x, v_y
-    if (pos[0] - pos_shaiba[0]) ** 2 + (pos[1] - pos_shaiba[1]) ** 2 <= 40 ** 2:
-        v_x = (-pos[0] + pos_shaiba[0]) // 5
-        v_y = (-pos[1] + pos_shaiba[1]) // 5
-    if (pos_vrag[0] - pos_shaiba[0]) ** 2 + (pos_vrag[1] - pos_shaiba[1]) ** 2 <= 40 ** 2:
-        v_x = (-pos_vrag[0] + pos_shaiba[0]) // 5
-        v_y = (-pos_vrag[1] + pos_shaiba[1]) // 5
+class Hokkey:  # аэрохокей
+    pass
 
 
+class Board:
+    pass
+    # создание поля
+
+
+class Minesweeper(Board):  # сапер
+    pass
+
+
+# hokkey.draw()
 pygame.display.flip()
 running = True
 clock = pygame.time.Clock()
@@ -32,6 +44,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    draw()
+
     pygame.display.flip()
-    clock.tick(24)
+    clock.tick(25)
+# print(time, comp, igr)
