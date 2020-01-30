2162f6ad550b3d5551cfb8c50d27ab9cb5aed6b7
katya1tch <echikireva@fmschool72.ru>
2020-01-30 15:30:04 +0500

    window rule

--- project.py
+++ project.py
@@ -1,100 +1,88 @@
 import pygame
 
+# инициализация Pygame:
 pygame.init()
+# размеры окна:
 size = width, height = 1100, 700
+# screen — холст, на котором нужно рисовать:
 screen = pygame.display.set_mode(size)
-pos = (400, 350 + 175)
-pos_vrag = (400, 175)
-pos_shaiba = (400, 350)
-x = 0
-v_x = 0
-v_y = 0
-
-schet_igr = 0
-schet_comp = 0
+color_end = (255, 255, 0)
+color_font = (255, 0, 0)
 
 
+# формирование кадра:
+# команды рисования на холсте
 def draw():
-    # screen.fill((204, 202, 202))  # светлая тема
-    screen.fill((57, 57, 57))  # темная тема
-    pygame.draw.rect(screen, pygame.Color(87, 87, 87), (150, 0, 500, 700), 0)
-    # pygame.draw.rect(screen, pygame.Color(230, 230, 230), (150, 0, 500, 700), 0)
-    pygame.draw.rect(screen, pygame.Color(147, 147, 147), (150, 340, 500, 20), 0)
-    pygame.draw.circle(screen, (147, 147, 147), (400, 350), 64)
-    pygame.draw.rect(screen, pygame.Color(255, 13, 0), (300, 680, 200, 20), 0)
-    pygame.draw.rect(screen, pygame.Color(51, 61, 255), (300, 0, 200, 20), 0)
+    screen.fill(color_end)
+    # шрифты
+    font1 = pygame.font.Font(None, 50)
+    font2 = pygame.font.Font(None, 30)
+
+    pygame.draw.rect(screen, color_font, (940, 640, 150, 50), 0)  # кнопка играть
+    text = font1.render('Играть', 1, color_end)
+    text_x = 940 + 75 - text.get_width() // 2
+    text_y = 640 + 25 - text.get_height() // 2
+    screen.blit(text, (text_x, text_y))
+
+    text = font1.render('Добро пожаловать в супер новую версию ваших любимых игр!', 1, color_font)
+    text_x = width // 2 - text.get_width() // 2
+    text_y = 25
+    screen.blit(text, (text_x, text_y))
+    text_y += text.get_height() + 20
+
+    text = font2.render('В данной программе вы можете играть в разные игры, при этом ваш счет всегда сохраняется.', 1,
+                        color_font)
+    text_x = width // 2 - text.get_width() // 2
+    screen.blit(text, (text_x, text_y))
+    text_y += text.get_height() + 12
+
+    sp = ['Слева и справа от стола для аэрохокея расположен счет аэрохокея и общий соответственно. Когда один ',
+          'из игроков достигнет 5 очков в аэрохокее, счет слева обновится, а на счете справа данному игроку',
+          'добавится 1 очко.', 'При победе в игре "Сапер", очко добавляется сразу в общий счет.']
+    for t in sp:
+        text = font2.render(t, 1, color_font)
+        screen.blit(text, (5, text_y))
+        text_y += text.get_height() + 12
+
+    text = font1.render('Правила игры в аэрохокей:', 1, color_font)
+    text_x = width // 2 - text.get_width() // 2
+    screen.blit(text, (text_x, text_y))
+    text_y += text.get_height() + 20
+
+    sp = ['Это спортивная игра, которая проводится на специальном столе небольших размеров с бортиками.',
+          'Суть игры весьма проста: вы должны забить максимальное количество голов в ворота противника. У вас ',
+          'будут свои ворота и клюшка красного цвета и одна шайба. Мышкой вы управляете клюшкой, ей вы ',
+          'можете отбивать шайбу. Обратите внимание, что вы  можете управлять клюшкой только на своей половине',
+          'игрового поля.']
+    for t in sp:
+        text = font2.render(t, 1, color_font)
+        screen.blit(text, (5, text_y))
+        text_y += text.get_height() + 12
+
+    text = font1.render('Правила игры в сапера:', 1, color_font)
+    text_x = width // 2 - text.get_width() // 2
+    text_y -= 20
+    screen.blit(text, (text_x, text_y))
+    text_y += text.get_height() + 20
+
+    sp = ['Перед вами поле 8х8. Сначала нажмите на произвольную клетку. Число в ячейке показывает, сколько мин ',
+          'скрыто вокруг данной клетки. Поле вокруг нее - это квадрат 3х3, в центре которого находится данная ',
+          'ячейка. Это число поможет понять, где находятся безопасные ячейки, а где находятся бомбы. Если рядом',
+          'с открытой ячейкой есть пустая клетка, то она откроется автоматически. Если вы открыли ячейку с миной,',
+          'все клетки с бомбами станут синими, а игра обновится, так как вы проиграли.',
+          'Игра продолжается до тех пор, пока вы не откроете все не заминированные ячейки.']
+    for t in sp:
+        text = font2.render(t, 1, color_font)
+        screen.blit(text, (5, text_y))
+        text_y += text.get_height() + 12
 
 
-def beat(): #otbit
-    global v_x, v_y
-    if (pos[0] - pos_shaiba[0]) ** 2 + (pos[1] - pos_shaiba[1]) ** 2 <= 40 ** 2:
-        v_x = (-pos[0] + pos_shaiba[0]) // 5
-        v_y = (-pos[1] + pos_shaiba[1]) // 5
-    if (pos_vrag[0] - pos_shaiba[0]) ** 2 + (pos_vrag[1] - pos_shaiba[1]) ** 2 <= 40 ** 2:
-        v_x = (-pos_vrag[0] + pos_shaiba[0]) // 5
-        v_y = (-pos_vrag[1] + pos_shaiba[1]) // 5
-
-
-def border(): #kraya
-    global v_y, v_x
-    if pos_shaiba[0] - 20 <= 150 or pos_shaiba[0] + 20 >= 150 + 500:
-        v_x *= -1
-    if pos_shaiba[1] - 20 <= 0 or pos_shaiba[1] + 20 >= 700:
-        v_y *= -1
-
-
-def gate(): #vorota
-    global schet_comp, schet_igr
-    if pos_shaiba[1] - 20 in range(20) or pos_shaiba[1] + 20 in range(680, 700):
-        if pos_shaiba[0] in range(150 + 100, 150 + 300):
-            if pos_shaiba[1] - 20 in range(20):
-                schet_igr += 1
-            else:
-                schet_comp += 1
-            returning()
-
-
-def returning(): #vozvrat
-    global pos, pos_vrag, pos_shaiba, v_y, v_x
-    pos = (400, int(350 * 1.5))
-    pos_shaiba = (400, 350)
-    pos_vrag = (400, 175)
-    v_x = v_y = 0
-    print(schet_igr, schet_comp)
-
-
+# ...
 draw()
+# смена (отрисовка) кадра:
 pygame.display.flip()
-running = True
-clock = pygame.time.Clock()
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            returning()
-    draw()
-    if pygame.mouse.get_focused():
-        if pygame.mouse.get_pos()[0] in range(150 + 20, 150 + 500 - 20) and pygame.mouse.get_pos()[1] in \
-                range(350 + 20, 700 - 20):
-            pos = pygame.mouse.get_pos()
-            pygame.mouse.set_visible(False)
-        else:
-            pygame.mouse.set_visible(True)
-    pygame.draw.circle(screen, (255, 61, 51), pos, 20)
-    pygame.draw.circle(screen, (255, 13, 0), pos, 10)
-    pygame.draw.circle(screen, (51, 61, 255), pos_vrag, 20)
-    pygame.draw.circle(screen, (0, 13, 255), pos_vrag, 10)
-    beat()
-    border()
-    gate()
-    pos_shaiba = (pos_shaiba[0] + v_x, pos_shaiba[1] + v_y)
-    if pos_shaiba[0] > pos_vrag[0]:
-        pos_vrag = (pos_vrag[0] + abs(v_x) // 3 * 2, pos_vrag[1])
-    elif pos_shaiba[0] < pos_vrag[0]:
-        pos_vrag = (pos_vrag[0] - abs(v_x) // 3 * 2, pos_vrag[1])
-    # pos_vrag = (pos_vrag[0] + v_x // 3, pos_vrag[1])
-    pygame.draw.circle(screen, (255, 255, 0), pos_shaiba, 20)
 
-    pygame.display.flip()
-    clock.tick(24)
+# ожидание закрытия окна:
+while pygame.event.wait().type != pygame.QUIT:
+    pass  # завершение работы:
+pygame.quit()
