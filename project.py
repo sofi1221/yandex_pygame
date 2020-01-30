ddf1157b8f92b7f3ff32725674c6eaa923e2d271
sofi1221 <sofi1221@mail.ru>
2020-01-30 14:06:16 +0500

    Cоздание поля

--- project.py
+++ project.py
@@ -28,8 +28,15 @@ class Hokkey:  # аэрохокей
 
 
 class Board:
-    pass
     # создание поля
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[0] * width for _ in range(height)]
+        # значения по умолчанию
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
 
 
 class Minesweeper(Board):  # сапер
